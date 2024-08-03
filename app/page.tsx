cdc74bd2358f55d1ce038896a1e5d92bfe8a5cb5
mauassenco <mau.assenco@gmail.com>
2024-08-03 02:24:34 -0300

    chore: add git hooks setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <h1 className="mb-10 flex bg-red-400 font-bold text-stone-300">
       FSW Barber Home
     </h1>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
